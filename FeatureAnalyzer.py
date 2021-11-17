86fec72fc6c73e6dbb3659666dc3593cfbcaee30
Schneider Marcell <78796219+Marci0707@users.noreply.github.com>
2021-11-17 21:07:32 +0100

    features done, splitting data

--- FeatureAnalyzer.py
+++ FeatureAnalyzer.py
@@ -50,7 +50,7 @@ class SignatureFeatureAnalyzer:
     def f11(self):  # Jerk rms
         return (self.df["abs_jerk"] ** 2).mean() ** 0.5
 
-    def f12(self):  #t(2nd pen_down) / duration
+    def f13(self):  # t(2nd pen_down) / duration
         pen_downs_found = 0
         for row_idx in range(len(self.df)):
             if self.df["state"].iloc[row_idx] == 0:
@@ -60,31 +60,84 @@ class SignatureFeatureAnalyzer:
 
         return 1  # just in case
 
-    def f13(self): #(avg abs(velocity)) / (maximum of Vx)
+    def f14(self):  # (avg abs(velocity)) / (maximum of Vx)
         return ((self.df["x_d"] ** 2 + self.df["y_d"] ** 2) ** 0.5).mean() / self.df["x_d"].max()
 
+    def delta(self, col_idx):
+        sum_ = 0
+        state_idx = 3
+        curr_max = self.df.loc[1][col_idx]
+        curr_min = self.df.loc[1][col_idx]
+        for record in self.df.iterrows():
+            if record[1][state_idx] == 0:
+                sum_ += curr_max - curr_min
+                curr_max = -np.inf
+                curr_min = np.inf
+            if record[1][col_idx] > curr_max:
+                curr_max = record[1][col_idx]
+            if record[1][col_idx] < curr_min:
+                curr_min = record[1][col_idx]
+
+    def f15(self):
+        A_min = (self.df['y'].max() - self.df['y'].min()) * (self.df['x'].max() - self.df['x'].min())
+        denominator = self.delta(0) * self.delta(1)
+        return A_min / denominator
+
+    def f7(self):
+        return self.calc_std_dev_in_col('y') / self.delta(1)
+
+    def f16_and_18(self):
+        for i in range(len(self.df) - 1, -1, -1):
+            if self.df['state'].iloc[i] == 0:
+                x_last_penup = self.df['x'].iloc[i]
+                y_last_penup = self.df['y'].iloc[i]
+                x_max = self.df['x'].max()
+                y_min = self.df['y'].min()
+                f16 = (x_last_penup - x_max) / self.delta(0)
+                f18 = (y_last_penup - y_min) / self.delta(1)
+                return f16, f18
+
+    def f17_and_f19(self):
+        for i in range(len(self.df)):
+            if self.df['state'].iloc[i] == 1:
+                x_first_pendown = self.df['x'].iloc[i]
+                y_first_pendown = self.df['y'].iloc[i]
+                x_max = self.df['x'].max()
+                y_min = self.df['y'].min()
+                f17 = (x_first_pendown - x_max) / self.delta(0)
+                f19 = (y_first_pendown - y_min) / self.delta(1)
+                return f17, f19
+
+    def f22(self):
+        indices = self.df.index[self.df['state'] == 0].tolist()
+        pendown_duration = 0
+        for i in indices:
+            pendown_duration += self.df['t'].loc[i + 1] - self.df['t'].loc[i]
+
+        return pendown_duration / self.calc_duration()
+
     def analyze(self):
-        values = np.zeros(100)
+        values = np.zeros(20)
         values[0] = self.calc_duration()
         values[1] = self.calc_pen_ups()
         values[2] = self.calc_sign_changes_in_col("x") + self.calc_sign_changes_in_col("y")
         values[3] = self.calc_avg_jerk()
         values[4] = self.calc_std_dev_in_col("y_d_d")
         values[5] = self.calc_std_dev_in_col("y_d")
-        # values[6] = TODO
+        values[6] = self.f7()
         values[7] = self.count_local_maxima_in_col("x")
         values[8] = self.calc_std_dev_in_col("x_d_d")
         values[9] = self.calc_std_dev_in_col("x_d")
         values[10] = self.f11()
         values[11] = self.count_local_maxima_in_col("y")
-        values[12] = self.f12()
-        values[13] = self.f13()
-        #values[14] = TODO
-        #values[15] = TODO
-        #values[17] = TODO
-        #values[18] = TODO
-        #values[19] = TODO
-        #values[20] = TODO
+        values[12] = self.f13()
+        values[13] = self.f14()
+        values[14] = self.f15()
+        values[15], values[17] = self.f16_and_18()
+        values[16], values[18] = self.f17_and_f19()
+        values[19] = self.f22()
+
+        return values
 
     def calc_sign_changes_in_col(self, col_name):
         col = self.df[col_name]
@@ -97,9 +150,5 @@ class SignatureFeatureAnalyzer:
     def get_feature_list(self):
         return self.analyze()
 
-    def calc_pen_ups(self):  # TODO nicer
-        c = 0
-        for point in self.df:
-            if point[4] == 0:
-                c += 1
-        return c
+    def calc_pen_ups(self):
+        return len(self.df.index[self.df['state'] == 0].tolist())
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 
+import numpy as np
+
 
 def fetch_user_data(user_id):
     all_signatures = []
@@ -18,11 +20,30 @@ def fetch_user_data(user_id):
 
     return all_signatures
 
+def split_data_set(signatures, labels, ratios):
+    indices = np.random.permutation(len(signatures))
+    l = len(signatures)
+    valid_start_idx = int(l*ratios[0])
+    test_start_idx = int(l*ratios[1])
+
+    X_train = signatures[indices[:valid_start_idx]]
+    y_train = labels[indices[:valid_start_idx]]
+    X_valid = signatures[indices[valid_start_idx:test_start_idx]]
+    y_valid = labels[indices[valid_start_idx:test_start_idx]]
+    X_test = signatures[indices[test_start_idx:]]
+    y_test = labels[indices[test_start_idx:]]
+
+    return (X_train,y_train), (X_valid,y_valid),(X_test,y_test)
+
+
+
 
 def main():
     user_id = input("What is the user id?")
     user_signatures = fetch_user_data(user_id)
-    print(len(user_signatures))
+    labels = [(x-1)//20 for x in range(1,40)]
+    (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = split_data_set(user_signatures,labels, ratios=(0.7, 0.15, 0.15))
+
 
 
 if __name__ == '__main__':
